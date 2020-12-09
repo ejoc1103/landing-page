85f261f0a3a2bc48cdba64646bb23d0bdd81c18e
Ed OConnor <ejoc1103@yahoo.com>
2020-12-09 15:29:14 -0500

    All regular requirements Complete. Working on collapsing sections

--- js/app.js
+++ js/app.js
@@ -1,74 +1,129 @@
-/**
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- */
-
-/**
- * Define Global Variables
- *
- */
-
+let sections = document.querySelectorAll("section");
+sections = Array.from(sections);
 const nav = document.querySelector("#navbar__list");
 const navSections = document.querySelectorAll("[data-nav] h2");
+const topOfPage = document.createElement("button");
+topOfPage.className = "button";
+const topButton = document.createTextNode("Top");
+topOfPage.appendChild(topButton);
+
+nav.insertAdjacentElement("afterend", topOfPage);
 
 //createNav
 for (let i = 0; i < navSections.length; i++) {
   const navListItem = document.createElement("li");
   const navLink = document.createElement("a");
   const navSection = document.createTextNode(navSections[i].textContent);
-  navLink.href = `index.html#section${i + 1}`;
   navLink.appendChild(navSection);
   navListItem.appendChild(navLink);
   nav.appendChild(navListItem);
 }
 
+sections.forEach((section, index) => {
+  let collapse = document.createTextNode("X");
+  let collapseButton = document.createElement("button");
+  collapseButton.appendChild(collapse);
+
+  if (index % 2 === 0) {
+    collapseButton.className = "collapseR";
+  } else {
+    collapseButton.className = "collapseL";
+  }
+  let sectionHeader = section.querySelector("h2");
+  sectionHeader.appendChild(collapseButton);
+});
+
 const navBar = document.querySelector(".navbar__menu");
 const navList = document.querySelector("#navbar__list");
-navBar.style.cssText =
-  "color: blue; font-size: 2em; font-weight: bold; padding: 10px;";
+navBar.style.className = "navbar__menu";
 const listItem = document.querySelectorAll("#navbar__list a");
 //style nav links
 for (let i = 0; i < navSections.length; i++) {
-  listItem[i].style.cssText = "color: orange; margin: 10px;";
+  listItem[i].className = "menu__link";
 }
 
-navBar.style.display = "none";
+navBar.style.display = "flex";
 
 document.addEventListener("scroll", function () {
-  if (window.scrollY >= 300) {
-    navBar.style.display = "flex";
-  } else {
+  navBar.style.display = "flex";
+  setTimeout(function () {
     navBar.style.display = "none";
+  }, 4000);
+});
+
+topOfPage.style.display = "none";
+
+document.addEventListener(
+  "scroll",
+  function () {
+    if (document.documentElement.scrollTop > window.innerHeight) {
+      topOfPage.style.display = "flex";
+    } else {
+      topOfPage.style.display = "none";
+    }
+  },
+  false
+);
+
+document.addEventListener("scroll", function () {
+  let sectionYs = [];
+  let activeSection = null;
+  let sectionYIndex = 0;
+
+  let activeMenu = document.querySelectorAll(".menu__link");
+  activeMenu = Array.from(activeMenu);
+  sections.forEach((section) => {
+    let y = section.getBoundingClientRect().y;
+    sectionYs.push(Math.abs(y));
+  });
+  sectionYs.forEach((section, index) => {
+    if (!activeSection || activeSection > section) {
+      activeSection = section;
+      sectionYIndex = index;
+    }
+  });
+  sections.forEach((section, index) => {
+    if (index === sectionYIndex) {
+      section.className = "your-active-class";
+      activeMenu[index].style.border = "4px solid orange";
+    } else {
+      section.classList.remove("your-active-class");
+      activeMenu[index].style.border = "";
+    }
+  });
+});
+
+let links = document.querySelectorAll("a");
+links.forEach((link, index) => {
+  const scrollTarget = sections[index].getBoundingClientRect().y;
+  link.addEventListener("click", function () {
+    scrollTo(0, scrollTarget);
+  });
+});
+
+topOfPage.addEventListener("click", function () {
+  scroll(0, 0);
+});
+
+document.addEventListener("mousemove", function (e) {
+  if (e.clientY < 100) {
+    navBar.style.display = "flex";
   }
 });
-/**
- * End Global Variables
- * Start Helper Functions
- *
- */
 
-/**
- * End Helper Functions
- * Begin Main Functions
- *
- */
+let collapseR = document.querySelectorAll(".collapseR");
+let collapseL = document.querySelectorAll(".collapseL");
 
-// build the nav
+collapseR.forEach((section) => {
+  section.addEventListener("click", function (e) {
+    let sectionId = e.path[3].id;
+    document.querySelector(`#${sectionId}`).style.display = "none";
+  });
+});
 
-// Add class 'active' to section when near top of viewport
-
-// Scroll to anchor ID using scrollTO event
-
-/**
- * End Main Functions
- * Begin Events
- *
- */
-
-// Build menu
-
-// Scroll to section on link click
-
-// Set sections as active
+collapseL.forEach((section) => {
+  section.addEventListener("click", function (e) {
+    let sectionId = e.path[3].id;
+    document.querySelector(`#${sectionId}`).style.display = "none";
+  });
+});
